bd0665ee3f1ddf66a3db74903d88566743e39b71
punnguyen1993 <punsky6789@gmail.com>
2022-02-05 19:06:01 +0700

    in progressing, error occurred

--- src/DashboardForm.java
+++ src/DashboardForm.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class DashboardForm extends JFrame {
     private JButton btnRegister;
@@ -15,7 +14,6 @@ public class DashboardForm extends JFrame {
         setContentPane(dashboardPanel);
         setMinimumSize(new Dimension(500, 450));
         setSize(700, 500);
-
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         boolean hasRegisteredUsers = connectToDatabse();
@@ -43,29 +41,44 @@ public class DashboardForm extends JFrame {
                 dispose();
             }
         }
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RegistationForm registationForm = new RegistationForm(DashboardForm.this);
+                User user = registationForm.user;
+
+                if(user != null){
+                    JOptionPane.showMessageDialog(DashboardForm.this,
+                            "New user: " + user.name,
+                            "Successful Registration",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
     }
+
 
     private boolean connectToDatabse() {
         boolean hasRegisteredUsers = false;
+        String MYSQL_SERVER_URL = "jdbc:mysql://localhost/";
+        String DB_URL = "jdbc:mysql://localhost/MyStore";// ở đây set time zone(?serverTimezone=UTC) phía sau
+                                                                // MyStore, chưa tìm hiểu nên bỏ.
+        String USERNAME = "root";
+        String PASSWORD = "th!nhnguyen";
 
-        final String MYSQL_SERVER_URL = "jdbc:mysql://localhost/";
-        final String DB_URL = "jdbc:mysql://localhoset/MyStore?serverTimezone=UTC";
-        final String USERNAME = "root";
-        final String PASSWORD = "th!nhnguyen93";
-        try{
-            //First, connect to MYSQL and create the database if not created
+        try {
             Connection conn = DriverManager.getConnection(MYSQL_SERVER_URL, USERNAME, PASSWORD);
             Statement statement = conn.createStatement();
-            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS MyStore");
-            statement.close();
+            String sql = "CREATE DATABASE IF NOT EXISTS MyStore";
+            statement.executeUpdate(sql);
             conn.close();
+            statement.close();
 
-            //Second, connect to the tdatabse and create the table "users" if not created
             conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             statement = conn.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS users(" +
-                    "id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-                    "name VARCHAR(200) NOT NULL," +
+            sql = "CREATE TABLE IF NOT EXISTS users(" +
+                    "id INT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR(200) NOT NULL" +
                     "email VARCHAR(200) NOT NULL UNIQUE," +
                     "phone VARCHAR(200)," +
                     "address VARCHAR(200)," +
@@ -73,19 +86,19 @@ public class DashboardForm extends JFrame {
                     ")";
             statement.executeUpdate(sql);
 
-            //check if we have users in the table users
             statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM users");
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM users;");
 
             if(resultSet.next()){
                 int numUsers = resultSet.getInt(1);
-                if(numUsers > 0){
+                if(numUsers>0){
                     hasRegisteredUsers = true;
                 }
             }
-            statement.close();
             conn.close();
-        }catch(Exception e){
+            statement.close();
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
